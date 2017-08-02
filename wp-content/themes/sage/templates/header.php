5240873b0fdc58da0acbc099c4c8cd5b7fecbc18
Aaron Frey <aaron.frey@gmail.com>
2017-08-01 21:16:53 -0400

    arrange header links

--- wp-content/themes/sage/templates/header.php
+++ wp-content/themes/sage/templates/header.php
@@ -1,12 +1,26 @@
 <header class="banner">
+
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+      <img
+        class="img-responsive img-circle"
+        src="<?php echo get_template_directory_uri() . '/dist/images/headshot-square.jpg'; ?>">
+    </a>
+
     <nav class="nav-primary">
+
+      <a href="<?= esc_url(home_url('/')); ?>">
+        <?php bloginfo('name'); ?>
+      </a>
+
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
       endif;
       ?>
     </nav>
+
   </div>
+
 </header>
