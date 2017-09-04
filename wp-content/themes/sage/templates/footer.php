aa5178cafdda553356f3a8fa65c455605de66e47
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 18:31:49 -0400

    make footer links one list

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -2,45 +2,15 @@
 
   <div class="container">
 
-    <div class="row">
-
-      <div class="col-sm-4">
-        <ul class="list-unstyled">
-          <li>
-            <a href="mailto:aaron.frey@gmail.com" target="_blank" title="Aaron's Email"><i class="fal fa-envelope"></i>Email</a>
-          </li>
-          <li>
-            <a href="https://stackoverflow.com/users/3084110/aaron-frey" target="_blank" title="Aaron's Stack Overflow"><i class="fab fa-stack-overflow"></i>Stack Overflow</a>
-          </li>  
-          <li>
-            <a href="https://www.linkedin.com/in/aaronjfrey" target="_blank" title="Aaron's LinkedIn"><i class="fab fa-linkedin"></i>LinkedIn</a>
-          </li>      
-        </ul>
-      </div>
-
-      <div class="col-sm-4">
-        <ul class="list-unstyled">     
-          <li>
-            <a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github"><i class="fab fa-github-alt"></i>Github</a>
-          </li>
-          <li>
-            <a href="https://trello.com/aaronjfrey" target="_blank" title="Aaron's Trello"><i class="fab fa-trello"></i>Trello</a>
-          </li>         
-        </ul>
-      </div>
-
-      <div class="col-sm-4">
-        <ul class="list-unstyled">
-          <li>
-            <a href="https://profiles.wordpress.org/aaronfrey" target="_blank" title="Aaron's Wordpress"><i class="fab fa-wordpress-simple"></i>Wordpress</a>
-          </li>        
-          <li>
-            <a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook"><i class="fab fa-facebook-square"></i>Facebook</a>
-          </li>
-        </ul>
-      </div>
-
-    </div>
+    <ul class="list-unstyled">
+      <li><a href="mailto:aaron.frey@gmail.com" target="_blank" title="Aaron's Email"><i class="fal fa-envelope"></i>Email</a></li>
+      <li><a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github"><i class="fab fa-github-alt"></i>Github</a></li>
+      <li><a href="https://stackoverflow.com/users/3084110/aaron-frey" target="_blank" title="Aaron's Stack Overflow"><i class="fab fa-stack-overflow"></i>Stack Overflow</a></li>  
+      <li><a href="https://www.linkedin.com/in/aaronjfrey" target="_blank" title="Aaron's LinkedIn"><i class="fab fa-linkedin"></i>LinkedIn</a></li>
+      <li><a href="https://trello.com/aaronjfrey" target="_blank" title="Aaron's Trello"><i class="fab fa-trello"></i>Trello</a></li>
+      <li><a href="https://profiles.wordpress.org/aaronfrey" target="_blank" title="Aaron's Wordpress"><i class="fab fa-wordpress-simple"></i>Wordpress</a></li>        
+      <li><a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook"><i class="fab fa-facebook-square"></i>Facebook</a></li>
+    </ul>
 
   </div>
 
