e080eb5adb75469e867209a9b8b6a3d27f7c3c99
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 14:12:47 -0400

    add favicons

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -3,6 +3,7 @@
   <div class="container">
 
     <div class="row">
+
       <div class="col-sm-4">
         <ul class="list-unstyled">
           <li>
@@ -17,6 +18,37 @@
           </li>
         </ul>
       </div>
+
+      <div class="col-sm-4">
+        <ul class="list-unstyled">
+          <li>
+            <a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github">
+              <i class="fab fa-github-alt"></i>Github
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook">
+              <i class="fab fa-facebook-square"></i>Facebook
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-sm-4">
+        <ul class="list-unstyled">
+          <li>
+            <a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github">
+              <i class="fab fa-github-alt"></i>Github
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook">
+              <i class="fab fa-facebook-square"></i>Facebook
+            </a>
+          </li>
+        </ul>
+      </div>
+
     </div>
 
   </div>
