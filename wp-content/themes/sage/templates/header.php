90cab6465b65e45fb75deeb28e92ae72bdeb5ca4
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 02:58:56 -0400

    reorganize header

--- wp-content/themes/sage/templates/header.php
+++ wp-content/themes/sage/templates/header.php
@@ -3,25 +3,21 @@
   <div class="container">
 
     <a class="brand" href="<?= esc_url(home_url('/')); ?>">
-
-      <div class="hover img-circle">
-        <div>Aaron<br><i class="far fa-code"></i><br>Frey</div>
-      </div>
-
-      <img
-        class="img-responsive img-circle"
-        src="<?php echo get_template_directory_uri() . '/dist/images/headshot-square.jpg'; ?>">
+      <img class="img-responsive img-circle" src="<?php echo get_template_directory_uri() . '/dist/images/headshot-square.jpg'; ?>">
     </a>
 
-    <nav class="nav-primary">
+    <div class="brand-text">
+      
+      <h2>Aaron Frey</h2>
+      <h3>Full Stack Developer</h3>
 
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+      <nav class="nav-primary">
+        <?php if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        endif; ?>
+      </nav>
 
-    </nav>
+    </div>
 
   </div>
 
