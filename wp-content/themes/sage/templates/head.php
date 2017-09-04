5465bc0fe7ad69eea575055589081a12e7b5cd1c
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 16:06:39 -0400

    add new fontawesome and add social links

--- wp-content/themes/sage/templates/head.php
+++ wp-content/themes/sage/templates/head.php
@@ -14,8 +14,8 @@
 	<meta name="theme-color" content="#ffffff">
 
 	<!-- fontawesome -->
- 	<script defer src="<?php echo get_template_directory_uri() . '/assets/scripts/fontawesome/fontawesome.js'; ?>"></script>
- 	<script defer src="<?php echo get_template_directory_uri() . '/assets/scripts/fontawesome/packs/brands.js'; ?>"></script>
-  <script defer src="<?php echo get_template_directory_uri() . '/assets/scripts/fontawesome/packs/regular.js'; ?>"></script>
+ 	<script defer src="<?php echo get_template_directory_uri() . '/assets/scripts/fontawesome/brands.js'; ?>"></script>
+  <script defer src="<?php echo get_template_directory_uri() . '/assets/scripts/fontawesome/light.js'; ?>"></script>
+  <script defer src="<?php echo get_template_directory_uri() . '/assets/scripts/fontawesome/fontawesome.js'; ?>"></script>
 
 </head>
