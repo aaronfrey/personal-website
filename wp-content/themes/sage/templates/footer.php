1d54dd6e1a9c7326441b9c21717b3cc82d38e967
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 16:27:13 -0400

    tweak container size

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -7,19 +7,13 @@
       <div class="col-sm-4">
         <ul class="list-unstyled">
           <li>
-            <a href="mailto:aaron.frey@gmail.com" target="_blank" title="Aaron's Email">
-              <i class="fal fa-envelope"></i>Email
-            </a>
+            <a href="mailto:aaron.frey@gmail.com" target="_blank" title="Aaron's Email"><i class="fal fa-envelope"></i>Email</a>
           </li>
           <li>
-            <a href="https://stackoverflow.com/users/3084110/aaron-frey" target="_blank" title="Aaron's Stack Overflow">
-              <i class="fab fa-stack-overflow"></i>Stack Overflow
-            </a>
+            <a href="https://stackoverflow.com/users/3084110/aaron-frey" target="_blank" title="Aaron's Stack Overflow"><i class="fab fa-stack-overflow"></i>Stack Overflow</a>
           </li>  
           <li>
-            <a href="https://www.linkedin.com/in/aaronjfrey" target="_blank" title="Aaron's LinkedIn">
-              <i class="fab fa-linkedin"></i>LinkedIn
-            </a>
+            <a href="https://www.linkedin.com/in/aaronjfrey" target="_blank" title="Aaron's LinkedIn"><i class="fab fa-linkedin"></i>LinkedIn</a>
           </li>      
         </ul>
       </div>
@@ -27,14 +21,10 @@
       <div class="col-sm-4">
         <ul class="list-unstyled">     
           <li>
-            <a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github">
-              <i class="fab fa-github-alt"></i>Github
-            </a>
+            <a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github"><i class="fab fa-github-alt"></i>Github</a>
           </li>
           <li>
-            <a href="https://trello.com/aaronjfrey" target="_blank" title="Aaron's Trello">
-              <i class="fab fa-trello"></i>Trello
-            </a>
+            <a href="https://trello.com/aaronjfrey" target="_blank" title="Aaron's Trello"><i class="fab fa-trello"></i>Trello</a>
           </li>         
         </ul>
       </div>
@@ -42,14 +32,10 @@
       <div class="col-sm-4">
         <ul class="list-unstyled">
           <li>
-            <a href="https://profiles.wordpress.org/aaronfrey" target="_blank" title="Aaron's Wordpress">
-              <i class="fab fa-wordpress-simple"></i>Wordpress
-            </a>
+            <a href="https://profiles.wordpress.org/aaronfrey" target="_blank" title="Aaron's Wordpress"><i class="fab fa-wordpress-simple"></i>Wordpress</a>
           </li>        
           <li>
-            <a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook">
-              <i class="fab fa-facebook-square"></i>Facebook
-            </a>
+            <a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook"><i class="fab fa-facebook-square"></i>Facebook</a>
           </li>
         </ul>
       </div>
