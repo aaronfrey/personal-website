5f185b0dd611570ddda1f3ae6b6c1f4ab7b8661a
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 21:09:40 -0400

    add a plugins page

---
+++ wp-content/themes/sage/page-plugins.php
@@ -0,0 +1,19 @@
+<?php while (have_posts()) : the_post(); ?>
+
+  <?php get_template_part('templates/content', 'page'); ?>
+
+  <div class="plugins-container">
+
+    <div class="row plugin">
+      <div class="col-md-12">
+        <h1><a href="https://wordpress.org/plugins/nearby-locations/" target="_blank">Nearby Locations</a></h1>
+        <div class="gallery">
+          <img class="img-responsive" src="<?php echo get_template_directory_uri() . '/dist/images/nearby-locations.png'; ?>">
+        </div>
+        <p>Nearby Locations is a Wordpress plugin that allows a user to enter multiple nearby locations, organized by type, that will be displayed around an optional central location.</p>
+      </div>
+    </div>
+
+  </div>
+
+<?php endwhile; ?>
