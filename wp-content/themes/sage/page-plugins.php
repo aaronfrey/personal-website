867d65d394cf2f18d5cab3613c4c73fe5d4ddc4f
Aaron Frey <aaron.frey@gmail.com>
2017-09-04 21:20:01 -0400

    add external tag to link

--- wp-content/themes/sage/page-plugins.php
+++ wp-content/themes/sage/page-plugins.php
@@ -7,7 +7,7 @@
     <div class="row plugin">
       <div class="col-md-12">
         <h1><a href="https://wordpress.org/plugins/nearby-locations/" target="_blank">Nearby Locations</a></h1>
-        <a class="gallery" href="https://wordpress.org/plugins/nearby-locations/">
+        <a class="gallery" href="https://wordpress.org/plugins/nearby-locations/" target="_blank">
           <img class="img-responsive" src="<?php echo get_template_directory_uri() . '/dist/images/nearby-locations.png'; ?>">
         </a>
         <p>Nearby Locations is a Wordpress plugin that allows a user to enter multiple nearby locations, organized by type, that will be displayed around an optional central location.</p>
