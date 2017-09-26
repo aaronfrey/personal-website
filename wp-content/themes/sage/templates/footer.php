6ba1235862456111b3f90e8c595f8f75a0d00f38
Aaron Frey <aaron.frey@gmail.com>
2017-09-26 12:57:26 -0400

    move to https

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -6,12 +6,12 @@
 
     <ul class="list-unstyled">
       <li><a href="mailto:<?php echo $email; ?>" target="_blank" title="Aaron's Email"><i class="fal fa-envelope"></i>Email</a></li>
-      <li><a href="https://github.com/aaronfrey" target="_blank" title="Aaron's Github"><i class="fab fa-github-alt"></i>Github</a></li>
-      <li><a href="https://stackoverflow.com/users/3084110/aaron-frey" target="_blank" title="Aaron's Stack Overflow"><i class="fab fa-stack-overflow"></i>Stack Overflow</a></li>  
-      <li><a href="https://www.linkedin.com/in/aaronjfrey" target="_blank" title="Aaron's LinkedIn"><i class="fab fa-linkedin"></i>LinkedIn</a></li>
-      <li><a href="https://trello.com/aaronjfrey" target="_blank" title="Aaron's Trello"><i class="fab fa-trello"></i>Trello</a></li>
-      <li><a href="https://profiles.wordpress.org/aaronfrey" target="_blank" title="Aaron's Wordpress"><i class="fab fa-wordpress-simple"></i>Wordpress</a></li>        
-      <li><a href="https://www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook"><i class="fab fa-facebook-square"></i>Facebook</a></li>
+      <li><a href="//github.com/aaronfrey" target="_blank" title="Aaron's Github"><i class="fab fa-github-alt"></i>Github</a></li>
+      <li><a href="//stackoverflow.com/users/3084110/aaron-frey" target="_blank" title="Aaron's Stack Overflow"><i class="fab fa-stack-overflow"></i>Stack Overflow</a></li>  
+      <li><a href="//www.linkedin.com/in/aaronjfrey" target="_blank" title="Aaron's LinkedIn"><i class="fab fa-linkedin"></i>LinkedIn</a></li>
+      <li><a href="//trello.com/aaronjfrey" target="_blank" title="Aaron's Trello"><i class="fab fa-trello"></i>Trello</a></li>
+      <li><a href="//profiles.wordpress.org/aaronfrey" target="_blank" title="Aaron's Wordpress"><i class="fab fa-wordpress-simple"></i>Wordpress</a></li>        
+      <li><a href="//www.facebook.com/aaronjfrey" target="_blank" title="Aaron's Facebook"><i class="fab fa-facebook-square"></i>Facebook</a></li>
       <li><a href="skype:aaron.j.frey" target="_blank" title="Aaron's Skype"><i class="fab fa-skype"></i>Skype</a></li>
     </ul>
 
