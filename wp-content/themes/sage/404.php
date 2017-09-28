bbb10bab0529efd3c5305fd874f3674c8b3939a3
Aaron Frey <aaron.frey@gmail.com>
2017-09-28 14:15:05 -0400

    Add new 404 page

--- wp-content/themes/sage/404.php
+++ wp-content/themes/sage/404.php
@@ -1,7 +1,6 @@
 <?php get_template_part('templates/page', 'header'); ?>
-
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
-
+<p>Vaya con Dios, bruh. This page isn't coming back. Johnny Utah knows how you feel.</p>
+<img class="img-responsive" style="width: 100%;" src="<?php echo get_template_directory_uri() . '/dist/images/giphy.gif'; ?>">
+<br>
+<p>Search for something else.</p>
 <?php get_search_form(); ?>
