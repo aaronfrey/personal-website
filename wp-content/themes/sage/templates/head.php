d8dd68c83185a1c18e4e26cbe3f07c446a57d00b
Aaron Frey <aaron.frey@gmail.com>
2017-09-28 13:18:23 -0400

    remove meta tag for development

--- wp-content/themes/sage/templates/head.php
+++ wp-content/themes/sage/templates/head.php
@@ -2,7 +2,6 @@
 
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
 
